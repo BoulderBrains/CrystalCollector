8ebf9a6936f0559ce8597fcc852a4188b845c677
Joshua <joshuadstarkey@gmail.com>
2018-10-10 00:24:27 -0700

    pseudo-code in place

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// create targetNumber and set it to a random number
+// display that targetVariable in the .target-number div
+// assign each crystal a point value
+// set up a user score variable set to 0
+// display the user score variable on the page
+// set up click handlers to increase userScore by the crystal point amount
+// if userScore === targetNumber { player wins, wins++, game restarts }
+// if userScore > targetNumber { player loses, losses++, game restarts }
+// On restart() {
+	// set a new targetNumber
+	// set new values to crystals
+	// userScore = 0; }
+// set up wins and losses variables
