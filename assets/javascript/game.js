816bacf87098dcc51413229a0387107ddafc66cc
Joshua <joshuadstarkey@gmail.com>
2018-10-12 15:30:59 -0700

    javascript in a better place

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,34 +5,32 @@ var losses = 0;
 $("#wins").append(wins);
 $("#losses").append(losses);
 
-// Trigger findTargerNumber function
-findTargetNumber();
-
 // set up a user score variable set to 0
 var userScore = 0;
-
-// display the user score variable on the page
-$(".score").append(userScore);
+var targetNumber = 0;
 
 // set up click handlers to increase userScore by the crystal point amount
-$(".crystal").click(function(userScore) {
+$(".crystal").click(function() {
+	console.log(userScore);
 	//get the value off a crystal
 	var crystalValue = $(this).val();
 	console.log(crystalValue + " crystalValue");
-	var updatedScore = parseInt(crystalValue) + userScore;
-	console.log(parseInt(updatedScore) + " updatedScore");
-	// display the updatedScore variable on the page
-	$(".score").html(updatedScore);
+	userScore = parseInt(crystalValue) + userScore;
+	console.log(parseInt(userScore) + " updatedScore");
+	// display the userScore variable on the page
+	$(".score").html(userScore);
 
 	// if userScore === targetNumber { player wins, wins++, game restarts }
-	if (userScore === findTargetNumber.targetNumber) {
+	if (userScore === targetNumber) {
 		wins++;
+		$("#wins").text(wins);
 		alert("you win");
 		reset();
 	}
 	// if userScore > targetNumber { player loses, losses++, game restarts }
-	if (userScore > findTargetNumber.targetNumber) {
+	if (userScore > targetNumber) {
 		losses++;
+		$("#losses").text(losses);
 		alert("you lose");
 		reset();
 	}
@@ -42,17 +40,21 @@ $(".crystal").click(function(userScore) {
 // On restart() {
 function reset() {
 	// find & set a new targetNumber
-	findTargetNumber(targetNumber);
+	generateTargetNumber();
 	// set userScore back to 0
 	userScore = 0;
+	// display the user score variable on the page
+	$(".score").text(userScore);
 }
-// set up wins and losses variables
 
 
-function findTargetNumber(targetNumber) {
+function generateTargetNumber() {
 	// create targetNumber and set it to a random number
-	var targetNumber = Math.floor((Math.random() * 99) + 1);
+	targetNumber = Math.floor((Math.random() * 99) + 1);
 
 	// display that targetVariable in the .target-number div
-	$(".target-number").append(targetNumber);
+	$(".target-number").text(targetNumber);
 }
+
+// This starts the game
+reset();
